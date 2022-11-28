0e8b30feb58113dad41b7a46ecc5c5377e6f1bd4
Siris <seeres60@gmail.com>
2022-11-28 17:07:06 +0545

    the targets are set and moving and gun is also set to point and click (initial commit)

---
+++ main.py
@@ -0,0 +1,125 @@
+import math
+import pygame
+
+pygame.init()
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('./assets/font/myFont.ttf',32)
+WIDTH = 900
+HEIGHT = 800
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+bgs=[]
+banners=[]
+guns=[]
+target_images = [[],[],[]]
+targets={
+    1:[10,5,3],
+    2:[12,7,5],
+    3:[15,10,8,3]
+}
+level = 3
+for i in range(1,4):
+    bgs.append(pygame.image.load(f'./assets/bgs/{i}.png'))
+    banners.append(pygame.image.load(f'./assets/banners/{i}.png'))
+    guns.append(pygame.transform.scale(pygame.image.load(f'./assets/guns/{i}.png'),(100,100)))
+    if i < 3:
+        for j in range(1,4):
+            target_images[i-1].append(pygame.transform.scale(pygame.image.load(f'./assets/targets/{i}/{j}.png'),(120 - (j*18), 80 - (j*12))))
+    else:
+        for j in range(1,5):
+            target_images[i-1].append(pygame.transform.scale(pygame.image.load(f'./assets/targets/{i}/{j}.png'),(120 - (j*18), 80 - (j*12))))
+
+def draw_level(coords):
+    if level == 1 or level == 2:
+        target_rect = [[],[],[]]
+    else:
+        target_rect = [[],[],[],[]]
+    for i in range(len(coords)):
+        for j in range(len(coords[i])):
+            target_rect[i].append(pygame.rect.Rect((coords[i][j][0] + 20 ,coords[i][j][1]),(60 - i*12, 60 - i*12)))
+            screen.blit(target_images[level-1][i], coords[i][j])
+    return target_images
+
+#now initialize enemy coordinates
+one_coords = [[],[],[]]
+two_coords = [[],[],[]]
+three_coords = [[],[],[],[]]
+for i in range(3):
+    my_list=targets[1]
+    for j in range(my_list[i]):
+        one_coords[i].append((WIDTH//(my_list[i])*j, (300-(i*150)+30 * (j % 2))))
+for i in range(3):
+    my_list=targets[2]
+    for j in range(my_list[i]):
+        two_coords[i].append((WIDTH//(my_list[i])*j, (300-(i*150)+30 * (j % 2))))
+for i in range(4):
+    my_list=targets[3]
+    for j in range(my_list[i]):
+        three_coords[i].append((WIDTH//(my_list[i])*j, (300-(i*100)+30 * (j % 2))))
+
+def draw_gun():
+    mouse_pos = pygame.mouse.get_pos()
+    gun_point = (WIDTH/2, HEIGHT-200)
+    lasers = ['red','purple','green']
+    clicks = pygame.mouse.get_pressed()
+    if mouse_pos[0]!=gun_point[0]: #for saving our butt when x2-x1 results to 0.
+        slope = (mouse_pos[1] - gun_point[1]) / (mouse_pos[0] - gun_point[0]) #just calculating the slope (y2-y1)/(x2-x1)
+    else:
+        slope = -100000  #randomly assigning negative large value.
+    angle = math.atan(slope)
+    rotation = math.degrees(angle)
+    if mouse_pos[0]< WIDTH/2:
+        gun = pygame.transform.flip(guns[level - 1],True,False) #True is flipping in x drection and False is flipping in y direction.
+        if mouse_pos[1] <= 600:
+            screen.blit(pygame.transform.rotate(gun, 90 - rotation), (WIDTH/2 - 90, HEIGHT-250))
+            if clicks[0]:
+                pygame.draw.circle(screen,lasers[level-1],mouse_pos, 5)
+    else:
+        gun = guns[level-1]
+        if mouse_pos[1] <= 600:
+            screen.blit(pygame.transform.rotate(gun, 270 - rotation), (WIDTH/2 - 30, HEIGHT-250))
+            if clicks[0]:
+                pygame.draw.circle(screen,lasers[level-1],mouse_pos, 5)
+
+def move_level(coords):
+    if level == 1 or level == 2:
+        max_val = 3
+    else:
+        max_val = 4
+    for i in range(max_val):
+        for j in range(len(coords[i])):
+            my_coords = coords[i][j]
+            if my_coords[0]< -150 :
+                coords[i][j] = (WIDTH, my_coords[1])
+            else:
+                coords[i][j] = (my_coords[0] - 2**i, my_coords[1])
+    return coords
+            
+
+run = True
+while run:
+    timer.tick(fps)
+
+    screen.fill("black")
+    screen.blit(bgs[level -1],(0,0))
+    screen.blit(banners[level -1],(0,HEIGHT-200))
+    
+    if level == 1:
+        target_boxes = draw_level(one_coords)
+        one_coords = move_level(one_coords)
+    if level == 2:
+        target_boxes = draw_level(one_coords)
+        two_coords = move_level(two_coords)
+    if level == 3:
+        target_boxes  = draw_level(three_coords)
+        three_coords = move_level(three_coords)
+
+    if level > 0 :
+        draw_gun();
+        # draw_level()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.flip() #take everything we mention about in above code and display it onto the screen.
+pygame.quit()
